da0fc2050c04244799ade446d13084809897e896
Ranjan <mahasethranjan004@.com>
2026-09-18 16:52:50 +0530

    fix express v5 wildcard route

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use("/api/user", userRouter);
 
 // Serve frontend
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
-app.get("*", (req, res) => {
+app.get("/{*path}", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
